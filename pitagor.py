484843031dfef102170e1738c97d7f11fa652d6c
Jose Luis <jluiserazod@yahoo.com.mx>
2016-08-17 13:02:19 -0500

    Se agrego el cuerpo de la tabla

--- pitagor.py
+++ pitagor.py
@@ -5,4 +5,11 @@ print("\t", end="")
 
 
 for i in range(n):
-	print("%d\t" %(i+1), end="")
+	print("%d\t" %(i+1), end="") 
+
+print("")
+
+for i in range(1, n+1):
+	print("%d\t"%i, end="")
+	for j in range(1,n+1): 
+		print("%d\t "%(i*j), end="")
